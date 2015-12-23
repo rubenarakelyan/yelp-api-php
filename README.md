2868faf54ed00e09c8b0ecbd90bddd497a81263a
Ruben Arakelyan <ruben@ra.me.uk>
2015-12-23 12:15:27 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# zomato-api-php
+
+PHP wrapper for the Yelp API.
+
+This wrapper is a simple extension to https://github.com/stevenmaguire/yelp-php
+that removes the default term and location attributes whicn prevent searching by
+latitude/longitude.
+
+## Support
+
+Please submit issues to https://github.com/rubenarakelyan/yelp-api-php/issues.
+
+## Contributing
+
+All pull requests are gratefully accepted.
+
+## Licence
+
+All files in this repository are licenced under the MIT licence.
+
+Please note that data pulled by the API is licenced separately.
---
+++ src/YelpApi.php
@@ -0,0 +1,32 @@
+<?php namespace RubenArakelyan\YelpApi;
+
+// **********************************************************************
+// Yelp API PHP wrapper
+// Version 1.0.0
+// Author: Ruben Arakelyan <ruben@ra.me.uk>
+//
+// Copyright (C) 2015 Ruben Arakelyan.
+// This file is licensed under the MIT licence.
+//
+// For more information, see https://github.com/rubenarakelyan/yelp-api-php
+// **********************************************************************
+
+/**
+ * Class YelpApi
+ * @package YelpApi
+ */
+class YelpApi extends Stevenmaguire\Yelp\Client
+{
+    public function buildQueryParams($attributes = [])
+    {
+        $defaults = [
+            'limit' => $this->searchLimit
+        ];
+        
+        $attributes = array_merge($defaults, $attributes);
+        
+        return http_build_query($attributes);
+    }
+}
+
+?>
